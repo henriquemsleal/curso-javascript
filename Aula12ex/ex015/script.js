b14afdcafe68ab31f57f17c3b74de5dd08de36e7
henriquemsleal <henriquemsleal@gmail.com>
2021-04-14 18:04:44 -0300

    atualizado ex015

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -9,12 +9,33 @@ function verificar() {
         var fSex = document.getElementsByName('radSex')
         var idade = ano - Number(fAno.value)
         var gen = ''
+        var img = document.createElement('img') //cria tag img
+        img.setAttribute('id', 'foto') //id para o elemento criado
         if (fSex[0].checked) {
             gen = 'Homem'
+            if (idade >= 0 && idade < 13) {
+                img.setAttribute('src', 'menino.png')
+            } else if (idade >= 13 && idade < 21) {
+                img.setAttribute('src', 'rapaz.png')
+            } else if (idade >=21 && idade < 60) {
+                img.setAttribute('src', 'homem.png')
+            } else {
+                img.setAttribute('src', 'senhor.png')
+            }
         } else if (fSex[1].checked) {
             gen = 'Mulher'
+            if (idade >= 0 && idade < 13) {
+                img.setAttribute('src', 'menina.png')
+            } else if (idade >= 13 && idade < 21) {
+                img.setAttribute('src', 'moca.png')
+            } else if (idade >=21 && idade < 60) {
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                img.setAttribute('src', 'senhora.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gen} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
