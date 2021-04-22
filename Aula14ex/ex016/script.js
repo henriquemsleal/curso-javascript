c33d9aaafaa0a0edaff28bb61778fff9d934fcb8
henriquemsleal <henriquemsleal@gmail.com>
2021-04-22 19:40:32 -0300

    atualizado ex016

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -3,23 +3,31 @@ function contar() {
     var fim = document.getElementById('cFim')
     var passo = document.getElementById('cPasso')
     var res = document.getElementById('res')
-    var vIni = ini.value
-    var vFim = fim.value
-    var vPasso = passo.value
+    res.innerHTML = ""
 
-    if (vIni.length == 0 || vFim.length == 0 || vPasso.length == 0) {
-        window.alert('[Erro] Faltam dados!')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        // window.alert('[Erro] Faltam dados!')
+        res.innerHTML = 'Impossível contar'
     } else {
-        res.innerHTML = `Contando: <br>`;
+        var vIni = Number(ini.value)
+        var vFim = Number(fim.value)
+        var vPasso = Number(passo.value)
+
+        if (vPasso == 0) {
+            res.innerHTML = '[Erro] O valor de Passo não pode ser 0! <br> Passo considerado como 1 <br><br>'
+            vPasso = 1
+        }
+        res.innerHTML += `Contando: <br>`;
+
         if (vIni < vFim) {
-            for (cont = Number(vIni); cont <= Number(vFim); cont += Number(vPasso)) {
+            for (let cont = vIni; cont <= vFim; cont += vPasso) {
                 res.innerHTML += ` ${cont} \u{1F449}`;
             }
-        } else if (vIni > vFim) {
-            for (cont = Number(vIni); cont >= Number(vFim); cont -= Number(vPasso)) {
+        } else if (vIni > vFim) { // poderia ser outro if
+            for (let cont = vIni; cont >= vFim; cont -= vPasso) {
                 res.innerHTML += ` ${cont} \u{1F449}`;
             }
         }
-        res.innerHTML += ` \u{1F3C1}`
     }
+    res.innerHTML += ` \u{1F3C1}`
 }
